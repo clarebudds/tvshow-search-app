9ba44b4ff68c5fead1935101818e8b191a6b2635
Clare <clarebudds@yahoo.co.uk>
2023-04-27 18:58:01 +0100

    Set up API call to TV Maze API and added async function

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const form = document.querySelector('#searchForm');
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    console.log(res.data);
 })
