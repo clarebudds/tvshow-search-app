965f999268f75f0d0da1b6fa18b5d659746f74fb
Clare <clarebudds@yahoo.co.uk>
2023-04-27 18:50:07 +0100

    Saved input value to a variable searchTerm

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('#searchForm');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log(form.elements.query.value)
+    const searchTerm = form.elements.query.value;
 })
